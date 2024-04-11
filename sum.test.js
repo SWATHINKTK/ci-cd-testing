2fba462109e8970614c8cc5747e12e1ab83a2e80
SWATHINKTK <swathinktk10@gmail.com>
2024-04-11 13:25:04 +0530

    add multiply

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,9 @@
-const sum = require('./sum');
+const calculations = require('./sum');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(calculations.sum(1, 2)).toBe(3);
 });
+
+test('multiply 2 * 2 to equal 4', () => {
+  expect(calculations.multiply(2,2)).toBe(4)
+})
